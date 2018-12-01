44c2d3d246874092587bca7e2ca613ffbd9b876b
p00rt <serzhooo@gmail.com>
2018-12-01 04:31:37 +0300

    Deploy again

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
   entry: { main: './src/index.js' },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
     filename: '[name].[chunkhash].js'
   },
   module: {
